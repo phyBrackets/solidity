c1d868dca1ae2fc690a9d2682e567fc08b69b34b
Shivam Rajput <75530356+mavish302@users.noreply.github.com>
2021-08-10 16:17:36 +0530

    Create interface.sol

---
+++ interface.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: GPL-3.0
+ 
+pragma solidity >=0.5.0 <0.9.0;
+
+interface BaseContract{
+  //  int public x;
+   // address public owner;
+    
+    //constructor(){
+      //  x=5;
+       // owner = msg.sender;
+   // }
+    
+    function setx(int _x) external ;
+}
+
+contract A is BaseContract{
+    int x;
+    int public y=3;
+    function setx(int _x)public override{
+        x=_x;
+    }
+}
